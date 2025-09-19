de35c9dc11c5836296d0232281b937da0acfc3cb
Charles <charlesroque0114@gmail.com>
2025-09-19 17:11:33 +0800

    Minor changes, set the Applicant's Guest view as the landing page

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // ✅ Import BrowserRouter
 import './index.css';
 import App from './App.jsx';
+import ApplicantGHome from './ApplicantGHome.jsx';
 // import ApplicantLogin from "./ApplicantLogin.jsx";
 // import HrHome from './HrHome';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter> {/* ✅ Wrap App with BrowserRouter once */}
-      <App />
+      <ApplicantGHome />
     </BrowserRouter>
   </StrictMode>
 );
